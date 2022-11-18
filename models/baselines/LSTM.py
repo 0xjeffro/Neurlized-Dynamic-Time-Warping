312ebe45fea70297511c413bb9c8f071224c81bc
Jeffro Tanvik <00002@protonmail.com>
2022-11-18 10:55:18 +0800

    update baseline lstm

---
+++ models/baselines/LSTM.py
@@ -0,0 +1,22 @@
+from torch import nn
+
+
+class LSTMClassification(nn.Module):
+
+    def __init__(self, input_dim, hidden_dim, target_size, num_layers=1, bidirectional=False, dropout=0.0):
+        super(LSTMClassification, self).__init__()
+        self.lstm = nn.LSTM(input_dim, hidden_dim, batch_first=True,
+                            num_layers=num_layers,
+                            bidirectional=bidirectional,
+                            dropout=dropout)
+        fc_dim = hidden_dim
+        if bidirectional is True:
+            fc_dim = fc_dim * 2
+        self.fc = nn.Linear(fc_dim, target_size)
+        self.softmax = nn.Softmax(dim=1)
+
+    def forward(self, input_):
+        lstm_out, (h, c) = self.lstm(input_.cuda())
+        logits = self.fc(lstm_out[:, -1])
+        scores = self.softmax(logits)
+        return scores
